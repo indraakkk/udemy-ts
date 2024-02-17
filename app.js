ab5aa7c37e228ce94ef319eedbab4a2ea5265821
indraakkk <indrakoslab@gmail.com>
2024-02-17 21:49:08 +0700

    feat: go through object types

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n1 !== 'number') {
-        throw new Error('invalid type');
+var Status;
+(function (Status) {
+    Status[Status["GUEST"] = 0] = "GUEST";
+    Status[Status["NOT_COMPLETE"] = 1] = "NOT_COMPLETE";
+    Status[Status["COMPLETE"] = 2] = "COMPLETE";
+})(Status || (Status = {}));
+var person = {
+    name: 'John',
+    age: 30,
+    hobbies: ['Sport', 'Reading'],
+    role: [2, 'author'],
+    status: Status.COMPLETE,
+};
+person.role = [0, 'admin'];
+var product = {
+    id: '123123',
+    price: 123123,
+    tags: ['new', 'hype'],
+    details: {
+        title: 'speed shoes',
+        description: 'new hype',
+    },
+};
+console.log(person.age);
+console.log(product.details.title);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    //        ^?
+    console.log(hobby.toUpperCase());
+    // since hobby type is a string, then ts show list of support function for the string
+    // if you force to access hobby.map(), ts will show error since this function not for string
+}
+if (person.status === Status.COMPLETE) {
+    console.log('resistration is complete');
+}
+// any *please avoid of this usage of any, breakage ts purpose
+var favoriteColor;
+favoriteColor = ['#974957', [22, 22, 22]];
+// union types
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
     }
     else {
-        return n1 + n2;
+        result = input1.toString() + input2.toString();
     }
+    return result;
 }
-var res = add(1, 3);
-//    ^?
-console.log("result is ".concat(add(1, 2)));
-console.log("result is ".concat(res));
+console.log(combine(10, 11));
+console.log(combine('10', 11));
--- app.ts
+++ app.ts
@@ -1,14 +1,74 @@
-function add(n1: number, n2:number){
-  if(typeof n1 !== 'number' || typeof n1 !== 'number'){
-    throw new Error('invalid type')
-  } else {
-    return n1+n2
-  }
+type Product = {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+};
+
+enum Status {
+  GUEST,
+  NOT_COMPLETE,
+  COMPLETE,
 }
 
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[]; // array of string
+  role: [number, string]; // tuple
+  status: Status;
+} = {
+  name: 'John',
+  age: 30,
+  hobbies: ['Sport', 'Reading'],
+  role: [2, 'author'],
+  status: Status.COMPLETE,
+};
 
-const res = add(1,3)
-//    ^?
+person.role = [0, 'admin'];
 
-console.log(`result is ${add(1,2)}`)
-console.log(`result is ${res}`)
+const product: Product = {
+  id: '123123',
+  price: 123123,
+  tags: ['new', 'hype'],
+  details: {
+    title: 'speed shoes',
+    description: 'new hype',
+  },
+};
+
+console.log(person.age);
+console.log(product.details.title);
+
+for (const hobby of person.hobbies) {
+  //        ^?
+  console.log(hobby.toUpperCase());
+  // since hobby type is a string, then ts show list of support function for the string
+  // if you force to access hobby.map(), ts will show error since this function not for string
+}
+
+if (person.status === Status.COMPLETE) {
+  console.log('resistration is complete');
+}
+
+// any *please avoid of this usage of any, breakage ts purpose
+let favoriteColor: any[];
+favoriteColor = ['#974957', [22, 22, 22]];
+
+// union types
+function combine(input1: number | string, input2: number | string) {
+  let result
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+console.log(combine(10, 11))
+console.log(combine('10', 11))
+
